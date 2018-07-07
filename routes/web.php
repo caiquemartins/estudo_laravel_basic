55386c14c6e337cd33a7bde6ef82de1961d11f47
caiquemart <caiquemart.cs@gmail.com>
2018-07-07 19:48:19 -0300

    Aula 8
    php artisan make:auth7

--- routes/web.php
+++ routes/web.php
@@ -20,21 +20,7 @@ Route::get('users', ['uses' => 'UsersController@index']);
 Route::get('users/create', ['uses' => 'UsersController@create']);
 
 Route::post('users', ['uses' => 'UsersController@store']);
-/*
-Route::get('users', function () {
-    $users = [
-        '0' => [
-            'first_name' => 'Renato',
-            'last_name' => 'Hysa',
-            'location' => 'Albania'   
-        ],
-        '1' => [
-            'first_name' => 'Jessica',
-            'last_name' => 'Alba',
-            'location' => 'USA'   
-        ],
-          
-    ];
-    return $users;
-});
-*/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
